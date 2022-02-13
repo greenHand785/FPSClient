f450171fe124496f859f3849475c35c980bebf65
greenHand785 <2311632028@qq.com>
2022-02-13 17:52:23 +0800

    项目添加

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("开始游戏游戏啦...");
+        UIManager.Instance.ShowPanel(PanelDefine.Panel_LoginPanel, true);
+        ClientInternetManger.Instance.StartConnection();
+    }
+}
